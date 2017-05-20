8252525e4917341c6db82e09c4fdbd00e5cdf266
Jiri Benc <jbenc@upir.cz>
2017-05-20 15:04:06 +0200

    added sha1 and base64

--- server/test.c
+++ server/test.c
@@ -1,7 +1,10 @@
 #include "test.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "base64.h"
 #include "common.h"
 #include "log.h"
+#include "sha1.h"
 #include "socket.h"
 
 struct conn_data {
@@ -10,6 +13,20 @@ struct conn_data {
 };
 
 static int streams = 0;
+
+static char *count_sha1(void *buf, size_t size)
+{
+	static char output[41];
+	SHA1_CTX sha;
+	unsigned char result[20];
+
+	SHA1Init(&sha);
+	SHA1Update(&sha, buf, size);
+	SHA1Final(result, &sha);
+	for (int i = 0; i < 20; i++)
+		snprintf(output + 2 * i, 3, "%02x", result[i]);
+	return output;
+}
 
 void *conn_new(struct socket *s __unused)
 {
@@ -32,9 +49,13 @@ void conn_read(struct socket *s, void *data)
 		count = socket_read(s, buf, 256);
 		if (!count)
 			break;
-		buf[count] = '\0';
+		//buf[count] = '\0';
 		cd->num_bytes += count;
-		log_info("stream %d: %s", cd->stream, buf);
+		log_info("stream %d sha: %s", cd->stream, count_sha1(buf, count));
+
+		unsigned char *b64 = base64_encode((unsigned char *)buf, count, NULL);
+		log_info("stream %d base64: %s", cd->stream, b64);
+		free(b64);
 	}
 	check(socket_write(s, "ack\n", 4, false));
 }
