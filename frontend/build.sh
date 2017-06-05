b7f11eaa4907a4e2eb81d6c0e25f9d8035f6091d
Jiri Benc <jbenc@upir.cz>
2017-06-06 00:26:54 +0200

    build.sh: oprava regexu
    Lomitko je jednim z vystupnich znaku base64. Jako oddelovac pro sed je tedy
    potreba pouzit jiny znak, napriklad zavinac.
    
    build.sh by navic mohl byt spustitelny.

--- frontend/build.sh
+++ frontend/build.sh
@@ -9,4 +9,4 @@ style="$(base64 -w 0 style.css)"
 mkdir -p build
 
 cp frontend.html build/index.html
-sed -i "s/script\.js/data:script\/javascript;base64,$script/; s/style\.css/data:text\/css;base64,$style/" build/index.html
+sed -i "s@script\.js@data:script/javascript;base64,$script@; s@style\.css@data:text/css;base64,$style@" build/index.html
